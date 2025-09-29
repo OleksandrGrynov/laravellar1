f6d6ab57b15a9a25e5383132530fd08fdfa89b91
Oleksandr Grynov <youremail@example.com>
2025-09-29 09:35:58 +0300

    all components

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,6 +1,6 @@
-<nav class="bg-light p-2">
-    <a href="{{ route('home') }}">Home</a> |
-    <a href="{{ route('about', ['mode' => 'two']) }}">About</a> |
-    <a href="{{ route('posts.index', ['mode' => 'one']) }}">Posts</a> |
-    <a href="{{ route('lab.index', ['mode' => 'three']) }}">Lab</a>
+<nav class="nav">
+    <a href="{{ route('home') }}" class="{{ request()->routeIs('home') ? 'active' : '' }}">Home</a>
+    <a href="{{ route('about', ['mode' => 'two']) }}" class="{{ request()->routeIs('about') ? 'active' : '' }}">About</a>
+    <a href="{{ route('posts.index', ['mode' => 'one']) }}" class="{{ request()->routeIs('posts.index') ? 'active' : '' }}">Posts</a>
+    <a href="{{ route('lab.index', ['mode' => 'three']) }}" class="{{ request()->routeIs('lab.index') ? 'active' : '' }}">Lab</a>
 </nav>
